df0cbbc1df8227024d58e785602b37073734c5c4
Felix <13115412392@163.com>
2019-08-22 17:21:16 +0800

    优化预加载

--- my-apps/src/App.js
+++ my-apps/src/App.js
@@ -114,8 +114,7 @@ class App extends Component {
   ,'https://s2.ax1x.com/2019/08/20/mJUS58.gif','https://s2.ax1x.com/2019/05/14/EoNJwn.png','https://s2.ax1x.com/2019/05/14/Eoa1Vs.png','https://s2.ax1x.com/2019/05/13/E5Jz6A.png','https://s2.ax1x.com/2019/05/13/E5hLAf.jpg','https://s2.ax1x.com/2019/05/13/E5hX4S.jpg','https://s2.ax1x.com/2019/05/13/E5hHBt.jpg',
 'https://s2.ax1x.com/2019/05/13/E5hON8.jpg','https://s2.ax1x.com/2019/05/13/E5hojA.png','https://s2.ax1x.com/2019/05/13/E5h7nI.png','https://s2.ax1x.com/2019/05/13/E5hbHP.png','https://s2.ax1x.com/2019/05/13/E5Uqud.png','https://s2.ax1x.com/2019/05/14/EISFyt.jpg','https://s2.ax1x.com/2019/05/14/EISZTS.jpg','https://s2.ax1x.com/2019/05/14/EISmFg.png',
 'https://s2.ax1x.com/2019/05/14/EISMSs.jpg','https://s2.ax1x.com/2019/05/14/EISuWj.jpg','https://s2.ax1x.com/2019/05/14/EIS1O0.png','https://s2.ax1x.com/2019/05/14/EISlyq.png','https://s2.ax1x.com/2019/05/13/E5w0AI.png','https://s2.ax1x.com/2019/05/14/EISBOx.jpg','https://s2.ax1x.com/2019/05/14/EISrm6.jpg','https://s2.ax1x.com/2019/05/14/EISs0K.jpg',
-'https://s2.ax1x.com/2019/05/14/EIS061.jpg','https://i.loli.net/2019/07/06/5d20377632d9f32987.png','https://i.loli.net/2019/07/06/5d203e0a1516c27087.jpg','https://i.loli.net/2019/07/06/5d203b7eb853054214.png','https://i.loli.net/2019/07/06/5d203b7eddbcd79081.png','https://i.loli.net/2019/07/06/5d203b7ef2f9412957.png','https://i.loli.net/2019/07/06/5d203b7e7be8b79536.png',
-'https://i.loli.net/2019/07/06/5d203b7e8476c91433.png','https://i.loli.net/2019/07/06/5d203b7ea9ba839733.png','https://i.loli.net/2019/07/06/5d203b7e9517290232.png','https://s2.ax1x.com/2019/08/20/mJwvQg.png','https://s2.ax1x.com/2019/08/20/mJDeaR.png','https://s2.ax1x.com/2019/08/20/mJDfzT.png','https://s2.ax1x.com/2019/08/20/mJD5yF.png','https://s2.ax1x.com/2019/08/20/mJD4QU.png',
+'https://s2.ax1x.com/2019/05/14/EIS061.jpg','https://s2.ax1x.com/2019/08/20/mJwvQg.png','https://s2.ax1x.com/2019/08/20/mJDeaR.png','https://s2.ax1x.com/2019/08/20/mJDfzT.png','https://s2.ax1x.com/2019/08/20/mJD5yF.png','https://s2.ax1x.com/2019/08/20/mJD4QU.png',
 'https://s2.ax1x.com/2019/08/20/mJrKTs.png','https://s2.ax1x.com/2019/05/13/E50XQS.png','https://s2.ax1x.com/2019/05/14/EISOpj.png','https://s2.ax1x.com/2019/05/14/EISqhQ.png','https://s2.ax1x.com/2019/05/14/EIpSBV.png','https://s2.ax1x.com/2019/05/14/EIpp7T.png','https://s2.ax1x.com/2019/05/14/EIpEcR.png','https://s2.ax1x.com/2019/05/14/EISzn0.png','https://s2.ax1x.com/2019/05/14/EIpenx.png',
 'https://s2.ax1x.com/2019/05/14/EIpVj1.png','https://s2.ax1x.com/2019/05/13/E5B4pV.png','https://s2.ax1x.com/2019/05/14/EIpsvn.jpg','https://s2.ax1x.com/2019/08/20/mJ6kf1.jpg','https://s2.ax1x.com/2019/05/14/EI9mxs.png','https://s2.ax1x.com/2019/05/14/EI9AIS.png','https://s2.ax1x.com/2019/05/14/EI9ka8.png','https://s2.ax1x.com/2019/05/14/EI9VPg.png','https://s2.ax1x.com/2019/05/14/EI9FVf.png',
 'https://s2.ax1x.com/2019/05/14/EI9ZGQ.png','https://s2.ax1x.com/2019/05/14/EI9e2j.png','https://s2.ax1x.com/2019/05/14/EI9PqP.png','https://s2.ax1x.com/2019/05/14/EI9JG4.jpg','https://s2.ax1x.com/2019/05/13/E5BLkR.png','https://s2.ax1x.com/2019/05/14/EI9TJg.png','https://s2.ax1x.com/2019/05/14/EI9heP.png','https://s2.ax1x.com/2019/05/14/EI9oFS.png','https://s2.ax1x.com/2019/05/14/EI9WLt.png',
@@ -131,11 +130,23 @@ class App extends Component {
         this.setState({
           per:parseInt((num / len)*100) +'%'
         })
+        console.log(i);
         console.log(this.state.per)
         if((num / len) == 1){
           this.setState({
             beginn:'beginn1'
           })
+          setInterval(() => {
+            axios({
+              method: "get",
+              url: "http://quan.suning.com/getSysTime.do"
+            }).then(res => {
+              // console.log(JSON.stringify(res.data.sysTime2));
+              this.setState({
+                date:res.data.sysTime2
+              })
+            });
+          }, 1000);
         }
       }, false);
   }
@@ -193,17 +204,7 @@ class Time extends Component{
     };
   }
   componentDidMount() {
-      setInterval(() => {
-        axios({
-          method: "get",
-          url: "http://quan.suning.com/getSysTime.do"
-        }).then(res => {
-          // console.log(JSON.stringify(res.data.sysTime2));
-          this.setState({
-            date:res.data.sysTime2
-          })
-        });
-      }, 1000);
+      
   }
   render(){
     return(
