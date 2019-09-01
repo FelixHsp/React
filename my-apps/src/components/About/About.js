e9ae8cdd9b4923b650ad3f4c44701572bf748ea2
Felix <13115412392@163.com>
2019-09-01 19:19:52 +0800

    0

--- my-apps/src/components/About/About.js
+++ my-apps/src/components/About/About.js
@@ -5,21 +5,21 @@ export default class About extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            wechat:"https://s2.ax1x.com/2019/05/14/EoaE5t.png",
-            qq:"https://s2.ax1x.com/2019/05/14/EoUD9f.png"
+            wechat:"https://s2.ax1x.com/2019/09/01/np7utK.png",
+            qq:"https://s2.ax1x.com/2019/09/01/np7ou9.png"
         }
         this.cwechate = this.cwechate.bind(this);
         this.cqq = this.cqq.bind(this)
     }
     cwechate(){
         this.setState({
-            qq:"https://s2.ax1x.com/2019/05/14/EoUD9f.png",
+            qq:"https://s2.ax1x.com/2019/09/01/np7ou9.png",
             wechat:"https://i.loli.net/2019/05/15/5cdbb3df2213d52243.jpg"
         })        
     }
     cqq(){
         this.setState({
-            wechat:"https://s2.ax1x.com/2019/05/14/EoaE5t.png",
+            wechat:"https://s2.ax1x.com/2019/09/01/np7utK.png",
             qq:"https://i.loli.net/2019/05/15/5cdbb3df22a0387743.jpg"
         })        
     }
@@ -28,7 +28,7 @@ export default class About extends Component {
         return ( 
             <div className="about">
                 <img src="https://s2.ax1x.com/2019/08/20/mJUS58.gif" className="gif" alt=""/>
-                <a href="https://github.com/FelixHsp" target="_blank"><img src="https://s2.ax1x.com/2019/05/14/EoNJwn.png" className="git" alt=""/></a>
+                <a href="https://github.com/FelixHsp" target="_blank"><img src="https://s2.ax1x.com/2019/09/01/np7KfO.png" className="git" alt=""/></a>
                 <img src={this.state.wechat} className="wechat" alt="" onClick={this.cwechate}/>
                 <img src={this.state.qq} className="qq" alt="" onClick={this.cqq}/>
                 <a href="https://blog.csdn.net/FelixHsp" target="_blank"><img src="https://s2.ax1x.com/2019/05/14/Eoa1Vs.png" className="csdn" alt=""/></a>
